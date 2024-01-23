c90a72b41c4a025ed9fac046071ce9e9ee08e277
Clinton David <cdavid@opml.co.uk>
2024-01-23 16:30:48 +0300

    d to e - sudo permissions..

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 fluidPage(
 
     # Application title
-    titlePanel("Project d dashboard"),
+    titlePanel("Project e dashboard"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
