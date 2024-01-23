3e07052fa01b89dc169a02e125b0d43b4183c729
Clinton David <cdavid@opml.co.uk>
2024-01-23 16:46:39 +0300

    tunnel issue

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 fluidPage(
 
     # Application title
-    titlePanel("Project xx dashboard"),
+    titlePanel("Project xxyy dashboard"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
