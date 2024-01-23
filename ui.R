100ac77fe4980c3cc7927f2f684330ade1da58d4
Clinton David <cdavid@opml.co.uk>
2024-01-23 11:27:14 +0300

    project A - B

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 fluidPage(
 
     # Application title
-    titlePanel("Project A dashboard"),
+    titlePanel("Project B dashboard"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
