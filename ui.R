55c7dfbd9b01a950fa7a36222de1848417d3e4a8
Clinton David <cdavid@opml.co.uk>
2024-07-01 14:35:10 +0300

    app title mod

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 fluidPage(
 
     # Application title
-    titlePanel("Project xxyyzz dashboard"),
+    titlePanel("Simple dashboard"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
