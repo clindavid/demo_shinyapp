12834987fb37c3f8349bac6d8ab54eef6b592f07
Clinton David <cdavid@opml.co.uk>
2024-07-01 18:53:08 +0300

    update #6

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 fluidPage(
 
     # Application title
-    titlePanel("Simple dashboard - update #5"),
+    titlePanel("Simple dashboard - update #6"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
