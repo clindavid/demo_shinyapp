eecbbc35e17df992909523b3323c9e4bd92515ff
Clinton David <cdavid@opml.co.uk>
2024-07-11 11:39:48 +0300

    log req ip

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 fluidPage(
 
     # Application title
-    titlePanel("simple Shiny App"),
+    titlePanel("Demo Shiny App"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
