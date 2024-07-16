1e6807019a6dc712e10c62c5e8a7acf58fea630a
Clinton David <cdavid@opml.co.uk>
2024-07-16 15:42:15 +0300

    another try - from main branch

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 fluidPage(
 
     # Application title
-    titlePanel("Demo Shiny-App(main)"),
+    titlePanel("Demo Shiny-App (from main branch)"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
