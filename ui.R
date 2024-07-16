ce2b6344bd1b79be036c374a22cd3096ac71f6d1
Clinton David <cdavid@opml.co.uk>
2024-07-16 16:55:32 +0300

    on dev server - test

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 fluidPage(
 
     # Application title
-    titlePanel("Demo Shiny-App (from main branch)"),
+    titlePanel("Demo Shiny-App : from main branch"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
