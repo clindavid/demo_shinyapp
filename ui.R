8c9e34a28085ce0ffe190a998adcda7570513bd1
Clinton David <cdavid@opml.co.uk>
2024-07-30 12:48:36 +0300

    test push to dev branch

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 fluidPage(
 
     # Application title
-    titlePanel("Demo Shiny-App (from clinton branch)"),
+    titlePanel("Demo Shiny-App"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
